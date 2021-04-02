9031df694f9564f83fe4724168b0edcc76f6d378
Greg Harris <73864182+GregHarris90@users.noreply.github.com>
2021-04-01 19:27:14 -0700

    added dish routes (#19)

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
 // add additional api routes
+const dishRoutes = require('./dishRoutes');
+
+router.use('/dishes', dishRoutes);
 
 module.exports = router;
