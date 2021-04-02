71daf9e9491accaa5295f115ad63d941a5893731
Amal Janabayev <amalj2426@gmail.com>
2021-04-01 19:37:33 -0700

    Created routes for homepage(login, foodie-signup, chef-signup) and added several handlebars files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,33 @@
 const router = require('express').Router();
 const path = require('path');
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/html/index.html"));
+const { Chef, Cuisine } = require('../models')
+
+router.get('/login', (req,res) => {
+    if (req.session.logged_in) {
+        res.redirect('/homepage');
+        return;
+    }
+
+    res.render('login')
 })
+
+router.get('/chef-signup', (req,res) => {
+    if (req.session.logged_in) {
+        res.redirect('/homepage');
+        return;
+    }
+
+    res.render('chef-signup')
+})
+
+router.get('/foodie-signup', (req,res)=>{
+    if (req.session.logged_in) {
+        res.redirect('/homepage');
+        return;
+    }
+
+    res.render('foodie-signup')
+})
+
 
 module.exports = router;
