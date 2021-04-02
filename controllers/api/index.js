2265a59dfc88462ce76ec4957e1d37279c6e3d04
Greg Harris <ghart.productions@gmail.com>
2021-04-01 20:12:43 -0700

    updated index.js routes & folders

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 // add additional api routes
-const dishRoutes = require('./dishRoutes');
+const chefRoutes = require('./chef-routes');
+const cuisineRoutes = require('./cuisine-routes');
+const dishRoutes = require('./dish-routes');
+const foodieRoutes = require('./foodie-routes');
+const orderRoutes = require('./order-routes');
 
+router.use('/chefs', chefRoutes);
+router.use('/cuisines', cuisineRoutes);
 router.use('/dishes', dishRoutes);
+router.use('/foodies', foodieRoutes);
+router.use('/orders', orderRoutes);
 
 module.exports = router;
