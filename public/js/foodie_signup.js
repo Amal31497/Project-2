df7db351328800fca6013fcd231b1308d1b04999
Hanh Le <hanhdtle@gmail.com>
2021-04-04 14:52:58 -0700

    got rid of last name and added foodie_signup.js

---
+++ public/js/foodie_signup.js
@@ -0,0 +1,32 @@
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+    console.log(" Click CVlikck");
+    const email = document.querySelector('#foodieEmail').value.trim();
+    const password = document.querySelector('#foodiePassword').value.trim();
+    const first_name = document.querySelector('#foodieFirstName').value.trim();
+    const phone_number = document.querySelector('#foodiePhone').value.trim();
+    const address = document.querySelector('#foodieStreet').value.trim();
+    const city = document.querySelector('#foodieCity').value.trim();
+    const state = document.querySelector('#foodieState').value.trim();
+    const zipcode = document.querySelector('#foodieZipcode').value.trim();
+
+    if (email && password) {
+      const response = await fetch('api/foodies', {
+        method: 'POST',
+        body: JSON.stringify({ email, password, first_name, phone_number, address, city, state, zipcode}),
+        headers: { 'Content-Type': 'application/json' },
+      });
+     
+  
+      if (response.ok) {
+        document.location.replace('/login');
+      } else {
+        alert( await response.statusText);
+      }
+    }
+};
+
+
+const submitButton = document.querySelector('#signupbtn');
+submitButton.addEventListener("click", signupFormHandler);
