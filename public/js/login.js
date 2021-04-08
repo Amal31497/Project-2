53734ca6efcea57e80a694fc9bde5910669d04a2
Amal Janabayev <amalj2426@gmail.com>
2021-04-08 00:22:35 -0700

    Fixed chef-page and everything is working now

--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
       
       if (response.ok) {
         // If successful, redirect the browser to the profile page
-        document.location.replace('/chef-profile');
+        document.location.replace('/chef-form');
         console.log(response)
       } else {
         alert(response.statusText);
